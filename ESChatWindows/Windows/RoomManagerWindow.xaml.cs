d02e50843eaaa5b046e03f38191676a220b51aba
Tomáš Švejnoha <tomas.svejnoha@gmail.com>
2018-01-17 19:40:18 +0100

    RoomManagerWindow created

---
+++ ESChatWindows/Windows/RoomManagerWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using ESChatWindows.Models.Server;
+using System.Windows;
+
+namespace ESChatWindows.Windows
+{
+    public partial class RoomManagerWindow : Window
+    {
+        public RoomManagerWindow()
+        {
+            InitializeComponent();
+        }
+        public RoomManagerWindow(Room room) : this()
+        {
+            this.Room = room;
+            this.WTBName.Text = room.Name;
+            this.WTBDescription.Text = room.Description;
+        }
+
+        public Room Room { get; protected set; }
+
+        private void ButtonCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+
+        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+            this.Room.Name = this.WTBName.Text;
+            this.Room.Description = this.WTBDescription.Text;
+            this.DialogResult = true;
+        }
+    }
+}
